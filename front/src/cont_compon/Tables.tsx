4ca11a83700d50976a7114e82999ad73899ca2b3
viktoria <vika63687@gmail.com>
2025-08-08 07:32:59 +0300

    Fix table partly

--- front/src/cont_compon/Tables.tsx
+++ front/src/cont_compon/Tables.tsx
@@ -13,16 +13,15 @@ import { useQuery } from "@tanstack/react-query";
 import { useMemo } from "react";
 // import { Filter } from "./filter";
 // import 'react-tabulator/css/tabulator.min.css';
-import { SlArrowDown, SlArrowUp} from "react-icons/sl";
-import type { RegattaContestRow } from "../client";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import type { ProblemResult, RegattaContestRow } from "../client";
 import Color from "colorjs.io";
 import { getRegattaContestStandingsOptions } from "../client/@tanstack/react-query.gen";
 import { useSearchParam } from "react-use";
 
-
 const columnHelper = createColumnHelper<RegattaContestRow>();
 
-const hightlighted_user_id = 20792; 
+const hightlighted_user_id = 20773;
 
 const columns = [
   columnHelper.accessor("user_id", {
@@ -48,28 +47,28 @@ const columns = [
 ];
 
 const teamColors = [
-  "#b1d0d9dd", // оригинальный
-  "#c7ceeadd", // оригинальный
-  "#A2AADBdd",   // оригинальный
-  "#F5CBCBdd",   // оригинальный
-  "#fabed4dd",   // оригинальный
-  "#FFEAEAdd",   // оригинальный
-  "#D4E6F1dd",   // светло-голубой
-  "#E8F5E9dd",   // мятно-зеленый
-  "#FFF3E0dd",   // теплый бежевый
-  "#FCE4ECdd",   // розоватый
-  "#E0F7FAdd",   // бирюзовый
-  "#F1F8E9dd",   // салатовый
-  "#FFE0B2dd",   // персиковый
-  "#D7CCC8dd",   // светло-коричневый
-  "#E1BEE7dd",   // лавандовый
-  "#C5CAE9dd",   // сиреневый
-  "#B2DFDBdd",   // морской волны
-  "#FFCCBCdd",   // коралловый
-  "#DCEDC8dd",   // светло-зеленый
-  "#F8BBD0dd",   // розовый
-  "#BBDEFBdd",   // васильковый
-  "#CFD8DCdd"    // серо-голубой
+  "#cdd7d9dd", // оригинальный
+  "#e1e4f2dd", // оригинальный
+  "#c9cce2dd", // оригинальный
+  "#F5CBCBdd", // оригинальный
+  "#f5dce5dd", // оригинальный
+  "#FFEAEAdd", // оригинальный
+  "#D4E6F1dd", // светло-голубой
+  "#E8F5E9dd", // мятно-зеленый
+  "#FFF3E0dd", // теплый бежевый
+  "#FCE4ECdd", // розоватый
+  "#E0F7FAdd", // бирюзовый
+  "#F1F8E9dd", // салатовый
+  "#f9eededd", // персиковый
+  "#D7CCC8dd", // светло-коричневый
+  "#E1BEE7dd", // лавандовый
+  "#C5CAE9dd", // сиреневый
+  "#B2DFDBdd", // морской волны
+  "#f8dfd7dd", // коралловый
+  "#DCEDC8dd", // светло-зеленый
+  "#fad8e4dd", // розовый
+  "#BBDEFBdd", // васильковый
+  "#CFD8DCdd", // серо-голубой
 ];
 function scoreToGreenColor(score: number) {
   if (score <= 0) return "transparent";
@@ -80,20 +79,26 @@ function scoreToGreenColor(score: number) {
   return vividGreen.to("srgb").toString({ format: "rgba" });
 }
 
-function colorTeam(row: RegattaContestRow, hightlighted_user_team_number: number | undefined) {
+function colorTeam(
+  row: RegattaContestRow,
+  hightlighted_user_team_number: number | undefined
+) {
   if (row.user_id === hightlighted_user_id) {
-      return {
-        backgroundColor: "#ff7700ff",
-        border: '2px solid #d9481fff',
-      }
-  } else if (hightlighted_user_team_number && row.team_number == hightlighted_user_team_number) {
-      return {
-        backgroundColor: "#ff7700ff",
-        // border: '1px solid #ee9900',
-      }
+    return {
+      backgroundColor: "#f25577ff",
+      border: "2px solid #eba6b3ff",
+    };
+  } else if (
+    hightlighted_user_team_number &&
+    row.team_number == hightlighted_user_team_number
+  ) {
+    return {
+      backgroundColor: "#f75e7fff",
+      // border: '1px solid #ee9900',
+    };
   }
   return {
-    backgroundColor: teamColors[(row.team_number - 1) % teamColors.length]
+    backgroundColor: teamColors[(row.team_number - 1) % teamColors.length],
   };
 }
 
@@ -106,59 +111,73 @@ const ResultsTable = () => {
   //   pageIndex: 0,
   //   pageSize: 100,
   // });
-  const contestId = parseInt(useSearchParam("contestId") || "")
-  console.log(contestId)
+  const contestId = parseInt(useSearchParam("contestId") || "");
 
-  const {data, isSuccess} = useQuery({
+  const { data, isSuccess } = useQuery({
     ...getRegattaContestStandingsOptions({
       path: {
-        contest_id: contestId
-      }
-    })
+        contest_id: contestId,
+      },
+    }),
   });
-  const tasks = useMemo(() => 
-    (isSuccess && data.rows) ? data.rows[0].problem_results.map(r => r.problem_code) : undefined, [data, isSuccess]); // TODO: fixme.
-  console.log("tasks: ", tasks);
-  const hightlighted_user_team_number = useMemo(() => 
-    (isSuccess && data.rows) && data.rows.find(r => r.user_id === hightlighted_user_id)?.    team_number || undefined,
-  [data, isSuccess]); // TODO: fixme.
-
+  const tasks = useMemo(() => {
+    return isSuccess &&
+      data.rows &&
+      data.rows.length > 0 &&
+      data.rows[0].problem_results
+      ? data.rows[0].problem_results.map((r) => r.problem_code)
+      : undefined;
+  }, [data, isSuccess]); // TODO: fixme.
+  const hightlighted_user_team_number = useMemo(
+    () =>
+      (isSuccess &&
+        data.rows &&
+        data.rows.find((r) => r.user_id === hightlighted_user_id)
+          ?.team_number) ||
+      undefined,
+    [data, isSuccess]
+  ); // TODO: fixme.
 
   const table = useReactTable({
     columns: [
       ...columns,
-      ...(tasks ? [
-        columnHelper.group({
-          id: "tasks",
-          header: () => "Задачи",
-          columns: tasks.map((taskName, i) =>
-            columnHelper.accessor(`problem_results`, {  // Доступ ко всему массиву
-              id: `task_${taskName}`, // Уникальный id для колонки
-              header: taskName,
-              cell: (props) => {
-                const problem = props.row.original.problem_results[i];
-                return (
-                  <div>
-                    {problem ? problem.score : "—"}
-                    {problem?.last_submission_time && (
-                      <i></i>
-                    // <div className={styles.taskTime}>
-                      //   {problem.last_submission_time}
-                      // </div>
-                    )}
-                  </div>
-                );
-              }
-            })
-          ),
-      })] : []),
+      ...(tasks
+        ? [
+            columnHelper.group({
+              id: "tasks",
+              header: () => "Задачи",
+              columns: tasks.map((taskName, i) =>
+                columnHelper.accessor(`problem_results`, {
+                  // Доступ ко всему массиву
+                  id: `task_${taskName}`, // Уникальный id для колонки
+                  header: taskName,
+                  cell: (props) => {
+                    const problem = props.row.original.problem_results[i];
+                    return (
+                      <div>
+                        {problem ? problem.score : "—"}
+                        {problem?.last_submission_time && (
+                          <>
+                            <div className={styles.taskTime}>
+                              {problem.last_submission_time}
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    );
+                  },
+                })
+              ),
+            }),
+          ]
+        : []),
     ],
     data: data?.rows || [],
     debugTable: true,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-      getFacetedMinMaxValues: getFacetedMinMaxValues(), //if you need min/max values
+    getFacetedMinMaxValues: getFacetedMinMaxValues(), //if you need min/max values
 
     // getPaginationRowModel: getPaginationRowModel(),
     // onPaginationChange: setPagination,
@@ -202,14 +221,14 @@ const ResultsTable = () => {
                   >
                     <div className={styles.headerColumn}>
                       <div>
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
                       </div>
                       {{
-                        asc: <SlArrowUp size="12px"/>,
-                        desc: <SlArrowDown size="12px"/>,
+                        asc: <SlArrowUp size="12px" />,
+                        desc: <SlArrowDown size="12px" />,
                       }[header.column.getIsSorted() as string] ?? null}
                     </div>
                     {/* {header.column.getCanFilter() ? (
@@ -225,23 +244,42 @@ const ResultsTable = () => {
         </thead>
         <tbody>
           {table.getRowModel().rows.map((row) => {
-            const rowHightlight = colorTeam(row.original, hightlighted_user_team_number)
+            const rowHightlight = colorTeam(
+              row.original,
+              hightlighted_user_team_number
+            );
             return (
               <tr key={row.id}>
                 {row.getVisibleCells().map((cell) => {
-                  let col;
-                  if (cell.column.id.startsWith("problem_results_")) {
-                    const minmaxValues = table.getColumn(cell.column.id)?.getFacetedMinMaxValues() as [number, number];
-                    col = scoreToGreenColor(cell.getValue() as number / minmaxValues[1])
-                  }
+                  const taskId = cell.column.id.replace("task_", "") // wtf
+                  const columnValues = table
+                    .getColumn(cell.column.id)
+                    ?.getFacetedRowModel()
+                    .rows.map(row => {
+                      const problemResults = row.original.problem_results;
+                      const problem = problemResults.find(p => p.problem_code === taskId);
+                      return problem?.score ?? 0;
+                    }) ?? [0];
+
+                    const minmaxValues = columnValues ? Math.max(...columnValues) : 0;
+
+
+                  const ddd = cell.getValue() as Array<ProblemResult> | undefined;
+                  const curScore = ddd && ddd.find && ddd.find(p => p.problem_code === taskId)?.score || 0;
                   return (
-                    <td key={cell.id} style={{
-                      backgroundColor: ["team_number", "display_name"].indexOf(cell.column.id) !== -1 ?
-                        rowHightlight.backgroundColor : 
-                        col,
-                      borderTop: rowHightlight.border,
-                      borderBottom: rowHightlight.border,
-                      }}>
+                    <td
+                      key={cell.id}
+                      style={{
+                        backgroundColor:
+                          ["team_number", "display_name"].indexOf(
+                            cell.column.id
+                          ) !== -1
+                            ? rowHightlight.backgroundColor
+                            : scoreToGreenColor(curScore / minmaxValues),
+                        borderTop: rowHightlight.border,
+                        borderBottom: rowHightlight.border,
+                      }}
+                    >
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
